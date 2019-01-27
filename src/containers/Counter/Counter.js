c59a9bfef8c95cf0d2af96f7eb145b3652dba456
Jerry <jerrybony@yahoo.com>
2019-01-27 16:44:02 -0600

    counter state manage by redux

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+    state = {
+        counter: 0
+    }
+
+    
+    render () {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+             //passing a payload everytime a dispatch is fired
+        onAddCounter: () => dispatch({type: 'ADD', val: 10}),
+        //passing a payload everytime a dispatch is fired
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15})
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
